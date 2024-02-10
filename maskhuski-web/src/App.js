eb1602d2c3f7c4ce55aa2e764a40d45251d925bb
neithanmervil <149252798+neithanmervil@users.noreply.github.com>
2024-02-10 00:33:51 -0500

    feat: [MSK][Neithan] Componente moneda y se agrego dos imagenes

--- maskhuski-web/src/App.js
+++ maskhuski-web/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Monedas from "./components/Monedas/Monedas";
 import Principal from "./pages/Principal/Principal";
 
 const App = () => {
   return (
     <div>
       <Principal />
+      <Monedas />
     </div>
   );
 };
