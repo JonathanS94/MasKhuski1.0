b17a9a64a7d15110b58228f449c8feb9133fa247
JonathanS94 <jonathan5445@live.com>
2024-02-09 23:50:19 -0500

    feat:[MSK-11][Jonathan]Modelar Interfaz Principal

--- maskhuski-web/src/App.js
+++ maskhuski-web/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Principal from "./pages/Principal/Principal";
+import Niveles from "./pages/Niveles/Niveles";
+import Principiante from "./pages/Principiante/Principiante";
+import Intermedio from "./pages/Intermedio/Intermedio";
 
 const App = () => {
   return (
-    <div>
-      <Principal />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Principal />} />
+        <Route path="/niveles" element={<Niveles />} />
+        <Route path="/niveles" element={<Principiante />} />
+        <Route path="/niveles" element={<Intermedio />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
