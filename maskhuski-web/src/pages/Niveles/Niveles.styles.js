ad794f5dbfd47239da0cec8bd75f787f8009bab8
neithanmervil <149252798+neithanmervil@users.noreply.github.com>
2024-02-11 00:09:03 -0500

    Feat:[MKS-12][Neithan] Interfaz de niveles

---
+++ maskhuski-web/src/pages/Niveles/Niveles.styles.js
@@ -0,0 +1,33 @@
+import { createUseStyles } from "react-jss";
+
+export const useStyles = createUseStyles({
+  
+  interfazNivelesContainer: {
+    margin: "10px",
+    textAlign: "center",
+    fontSize: "35px",
+    flexDirection: "column",
+    display: "flexDirection"
+  },
+
+  buttonPrincipiante: {
+    width: "180px",
+    height: "150px",
+    textAlign: "center",
+    fontWeight: "bold",
+    border: "1px solid white",
+    backgroundColor: "#002eab",
+    color: "white",
+  },
+
+  buttonIntermedio: {
+    width: "180px",
+    height: "150px",
+    textAlign: "center",
+    fontWeight: "bold",
+    border: "1px solid white",
+    backgroundColor: "#002eab",  
+    color: "white",   
+  },
+
+});
