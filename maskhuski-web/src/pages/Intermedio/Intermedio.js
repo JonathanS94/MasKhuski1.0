11fc1450aef3be4fcfb71fdfb867a7d05a4d5af5
JonathanS94 <jonathan5445@live.com>
2024-02-12 23:12:09 -0500

    feat:[MSK-14][Jonathan]Modelar Interfaz Intermedio

--- maskhuski-web/src/pages/Intermedio/Intermedio.js
+++ maskhuski-web/src/pages/Intermedio/Intermedio.js
@@ -1,12 +1,62 @@
-import React from "react";
-//import Typography from "../../common/Typography/Typography";  desimport lueguito
-import Image from "../../common/Image/Image";
-//import Button from "../../common/Button/Button"; desimport lueguito
+import React, { useState, useEffect } from "react";
+import Typography from "../../common/Typography/Typography";
+import Button from "../../common/Button/Button";
+import Billetes from "../../components/Billetes/Billetes";
+import { useStyles } from "./Intermedio.stlyes";
 
 const Intermedio = () => {
+  const classes = useStyles();
+  const [numbers, setNumbers] = useState([]);
+  useEffect(() => {
+    const initialNumbers = ["$4", "$7", "$15", "$60", "$30", "$70"];
+    const shuffledNumbers = shuffleArray(initialNumbers);
+    setNumbers(shuffledNumbers);
+  }, []);
+
+  // Función para mezclar un array
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
   return (
-    <div>
-      <Image></Image>
+    <div className={classes.container}>
+      <div style={{ fontSize: "36px" }}>
+        <Typography level={"h2"} children={"NIVEL INTERMEDIO"} />
+      </div>
+      <div>
+        <table className={classes.table}>
+          <tbody>
+            <tr>
+              <td className={classes.cell}>{numbers[0]}</td>
+              <td className={classes.cell}>{numbers[1]}</td>
+              <td className={classes.cell}>{numbers[2]}</td>
+            </tr>
+            <tr>
+              <td className={classes.cell}>{numbers[3]}</td>
+              <td className={classes.cell}>{numbers[4]}</td>
+              <td className={classes.cell}>{numbers[5]}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br></br>
+      <div>
+        <Billetes />
+        <br></br>
+      </div>
+      <Button
+        className={classes.button}
+        variant={"contained"}
+        value={"Enviar Respuesta"}
+      />
     </div>
   );
 };
